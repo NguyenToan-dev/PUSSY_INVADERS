f362d3925f37057ff4f25699370af6c3241b743c
Nguyen Khanh Toan <tn421015@gmail.com>
2025-07-15 12:52:41 +0700

    Update Pickup.h

--- PUSSY_INVADERS/src/Pickup.h
+++ PUSSY_INVADERS/src/Pickup.h
@@ -1,4 +1,4 @@
-﻿//Pickup.h
+//Pickup.h
 #pragma once
 #include <raylib.h>
 #include <vector>
@@ -6,14 +6,20 @@
 enum class PickupType {
     Sushi,
     Battery,
-    Milk
+    Milk,
+    Gift1,
+    Gift2,
+    Gift3
 };
+
 
 class Pickup {
 public:
     Pickup(PickupType t, const Vector2& at);
     // spawn một pickup tại vị trí at
     static void Spawn(const Vector2& at);
+    //spawn gift sau 15s
+    static void SpawnGift();
 
     // update tất cả – gọi trong GameController::Update()
     static void UpdateAll(float dt);
@@ -22,8 +28,6 @@ public:
     static void DrawAll();
 
 private:
-
-
     // instance update & draw
     void Update(float dt);
     void Draw() const;
@@ -34,10 +38,13 @@ private:
     float      lifeTime;
     bool       hasBounced;
     PickupType type;
+    float rotationAngle;  // góc xoay hiện tại (theo độ)
 
     // container và textures chung
     static std::vector<Pickup> pickups;
     static Texture2D           texSushi, texBattery, texMilk;
+    static Texture2D texGift1, texGift2, texGift3;
+    static float giftTimer;
     static bool                isLoaded;
     // để đảm bảo battery chỉ rớt 1 lần
     static bool                batteryDropped;
