6d4ef445f51241eee9c96573fd1d12b8172c7403
Nguyen Khanh Toan <tn421015@gmail.com>
2025-07-15 13:31:55 +0700

    Update SpaceShip.h

--- PUSSY_INVADERS/src/SpaceShip.h
+++ PUSSY_INVADERS/src/SpaceShip.h
@@ -1,3 +1,4 @@
+//SpaceShip.h
 #pragma once
 #include<raylib.h>
 #include<vector>
@@ -19,6 +20,12 @@ private:
     ull score;
     static int heat_limit;
 
+    int sushi_collected = 0;
+    int milk_collected = 0;
+    int battery_collected = 0;
+    bool isNewBullet = false;
+
+
     Brightness fireball_brightness;
     SpaceShipTexture image;
     Font font;
@@ -38,9 +45,9 @@ public:
     Vector2 BoundChecking(Vector2);
     Vector2 RocketPosition(Vector2);
     void Shooting(std::vector<Bullet>& bullets, Texture2D* bulletTexture);
-    
+
     Rectangle getRect();
     int HitBoxChecking(vector<Bullet*>&);
     void AdjustStatus(ShipStatus);
-
+    void EatPickup(); // Gọi mỗi frame trong GameController::Update(), hoặc bên trong ship.Update()
 };
