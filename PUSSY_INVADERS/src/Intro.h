debf098cc8f94c3326099b61f79c295294230740
NguyenToan-dev <tn421015@gmail.com>
2025-08-05 15:21:16 +0700

    update intro

---
+++ PUSSY_INVADERS/src/Intro.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+#include "raylib.h"
+#include <string>
+#include <vector>
+#include <sstream>
+struct IntroScene {
+    std::string imagePath;
+    std::string text;
+};
+
+class Intro {
+public:
+    Intro();
+    ~Intro();
+
+    void Update();
+    void Draw();
+    bool IsFinished() const;
+    Music getMusic() const { return music; }
+private:
+    void LoadScenes();
+    void AdvanceScene();
+    void DrawMultilineText(const std::string& text, Vector2 pos, float maxWidth, float fontSize, float spacing, Color color);
+
+    std::vector<IntroScene> scenes;
+    std::vector<Texture2D> images;
+    Music music;
+    Sound meowSound;
+    Font font;
+
+    int currentScene = 0;
+    float sceneTimer = 0.0f;
+    float sceneDuration = 7.0f;
+    bool finished = false;
+    bool allowSkipButton = false;
+    bool meowPlayed = false;
+    bool waiting = false; 
+    float waitTimer = 0.0f; 
+};
