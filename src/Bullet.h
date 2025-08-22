ed48d4f79b6ae539cbd412bdbe4b6243bfe7fa8a
Loc Vo <vdloc2431@clc.fitus.edu.vn>
2025-08-22 22:51:41 +0700

    resolve merge conflict with code Toan

--- src/Bullet.h
+++ src/Bullet.h
@@ -2,12 +2,7 @@
 #include "raylib.h"
 
 class Bullet {
-public:
-    Vector2 position;
-    float speed;
-    bool active;
-    Texture2D* texture;
-    int damage;  
+public:    
     Bullet(Vector2 pos, Texture2D* tex);
     virtual void Update();
     virtual void Draw();
@@ -26,7 +21,7 @@ public:
     void SetSpeed(float s) { speed = s; }
     void SetDamage(int dmg) { damage = dmg; }
 
-protected:
+public:
     Vector2 position;
     float speed;
     bool active;
--- src/Pussy.h
+++ src/Pussy.h
@@ -1,5 +1,4 @@
 #pragma once
-<<<<<<< HEAD:src/Pussy.h
 #include "PussyBase.h"
 
 class Pussy : public PussyBase {
@@ -14,27 +13,3 @@ private:
     static Texture2D image;
     Texture2D& getTexture() override;
 };
-=======
-#include <raylib.h>
-
-class Pussy {
-public:
-    Pussy(int type, Vector2 position);
-    void Update(int direction);
-    void Draw();
-    int GetType();
-
-    static int pussyDirection;
-    static float pussyShootTimer;
-    static float pussyShootInterval;
-
-    Rectangle getRect();
-
-    static void LoadImage();     
-    static void UnloadImage();   
-
-    static Texture2D image;     
-    Vector2 position;
-    int type;
-};
->>>>>>> Toan:PUSSY_INVADERS/src/Pussy.h
